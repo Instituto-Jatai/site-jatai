460a968d36a3ac8d3d12cb7ba6d30cab00a2519f
IAGO MENDES <iagommendes@gmail.com>
2022-11-14 18:52:48 -0300

    feat(JATAI-00): Starting up Jatais web page

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#00A4EA",
+        "primary-dark": "#273444",
+        "primary-transparent": "#00a4ea0d",
+        "gray-transparent": "#000000b3",
+        "white-transparent": "#ffffffcc",
+      },
+      container: {
+        center: true,
+      },
+      spacing: {
+        "92px": "92px",
+        "124px": "124px",
+      },
+      margin: {
+        "92px": "92px",
+        "124px": "124px",
+      },
+    },
+    fontFamily: {
+      sans: ["Epilogue"],
+      serif: ["Epilogue"],
+      mono: ["Epilogue"],
+      display: ["Epilogue"],
+      body: ["Epilogue"],
+    },
+  },
+  plugins: [],
+};
