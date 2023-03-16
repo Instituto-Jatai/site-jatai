1e723f35ddde975f5bcb48c499f4a16311da1009
hboppe <hanna.nascimento31@gmail.com>
2023-03-16 19:50:53 -0400

    fix: add color over hero on prediagnosis page

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,18 +9,20 @@ module.exports = {
         "primary-transparent": "#00a4ea0d",
         "gray-transparent": "#000000b3",
         "white-transparent": "#ffffffcc",
+        "teste": "rgba(20, 31, 25, 0.5)",
       },
       container: {
         center: true,
       },
       spacing: {
+        "24px": "24px",
         "92px": "92px",
         "124px": "124px",
       },
       margin: {
         "92px": "92px",
         "124px": "124px",
-      },
+      }
     },
     fontFamily: {
       sans: ["Inter"],
